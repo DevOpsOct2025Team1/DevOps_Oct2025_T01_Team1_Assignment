55f852f49210462b8749e22cb5e0aa2715a2bd9c
Raymond Goo <raymond.goo124@gmail.com>
2026-02-08 12:19:14 +0800

    fix: add allow origin using env.

--- apps/api-gateway/internal/server/server.go
+++ apps/api-gateway/internal/server/server.go
@@ -23,7 +23,7 @@ func New(authClient handlers.AuthServiceClient, userClient handlers.UserServiceC
 	router.Use(otelgin.Middleware("api-gateway"))
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:5173", cfg.FrontendURL},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
